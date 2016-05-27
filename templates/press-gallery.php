3201d4f9a196834fd99afaf4d4d680c79ed2ad7f
Sunil Williams <sunil@sunil.co.nz>
2016-05-27 21:29:27 +1200

    add styling for tablet width and wider

--- templates/press-gallery.php
+++ templates/press-gallery.php
@@ -6,9 +6,18 @@ $args = array(
 $my_query = new WP_Query($args) ;
  if ($my_query->have_posts()) : ?>
     <?php $count = 0 ;  ?>
-    <section id="dra-press-gallery">
-	<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-	    <div class="preview-image-container"  data-count="<?php $count  ?>">
+    <section id="dra-press-gallery" class="clearfix">
+	<?php while ($my_query->have_posts()) : $my_query->the_post(); 
+	$count++ ;	
+	$oddeven = '' ;
+	if ($count % 2 == 0) {
+	    $oddeven = "even" ;	    
+	} else {
+	    $oddeven = "odd";
+	}
+	?>
+	    
+	    <div class="preview-image-container <?php echo $oddeven  ?>">
 		<div class="inner">
 		<a href="<?php echo types_render_field( 'image', array( "alt" => "#", "output" => "raw") ) ;?>" class="block-link"  target="_blank">
 		    <?php echo types_render_field( 'image', array( "alt" => "#",  "size" => "han-preview-size" ) ) ;?>
