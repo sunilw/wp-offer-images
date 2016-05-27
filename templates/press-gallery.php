356cfba0c29cfb47f56b8e9bb1ad5a1b03d49886
Sunil Williams <sunil@sunil.co.nz>
2016-05-27 22:29:46 +1200

    make last row end on odd number and alter image sizing

--- templates/press-gallery.php
+++ templates/press-gallery.php
@@ -4,29 +4,45 @@ $args = array(
     'posts_per_page' => 8
 ) ;
 $my_query = new WP_Query($args) ;
- if ($my_query->have_posts()) : ?>
-    <?php $count = 0 ;  ?>
-    <section id="dra-press-gallery" class="clearfix">
-	<?php while ($my_query->have_posts()) : $my_query->the_post(); 
-	$count++ ;	
-	$oddeven = '' ;
-	if ($count % 2 == 0) {
-	    $oddeven = "even" ;	    
-	} else {
-	    $oddeven = "odd";
-	}
-	?>
+if ($my_query->have_posts()) :
+            $count = 0 ;
+$post_count = $my_query->post_count ;
+$remaining = $post_count ;
+?>
+    <section id="dra-press-gallery" class="clearfix">	
+        <?php while ($my_query->have_posts()) : $my_query->the_post();
+        $count++ ;
+        $oddeven = '' ;
+        if ($count % 2 == 0) {
+            $oddeven = "even" ;
+        } else {
+            $oddeven = "odd";
+        }
+        ?>
+
+            <div class="image-row clearfix">
+
+                <div class="preview-image-container <?php echo $oddeven  ?>"
+                     data-remaining-posts="<?php echo $remaining  ?>"
+                     data-total-posts="<?php echo $post_count  ?>"
+                >
+                    <div class="inner">
+                        <a href="<?php echo types_render_field( 'image', array( "alt" => "#", "output" => "raw") ) ;?>" class="block-link"  target="_blank">
+                            <?php echo types_render_field( 'image', array( "alt" => "#",  "size" => "han-preview-size" ) ) ;?>
+                        </a>
+                        <div class="caption-wrap">
+                            <p style="color: #000 ;"><?php  echo types_render_field('caption')  ?></p>
+                        </div>
+                    </div>
+                </div> <!-- ENDS .preview-image-container -->
+                <?php $remaining--  ?>
+                <?php
+                if ( $oddeven == "even"  || $remaining = "0" ) { ?>
+
+            </div>  <!-- ENDS .image-row -->
 	    
-	    <div class="preview-image-container <?php echo $oddeven  ?>">
-		<div class="inner">
-		<a href="<?php echo types_render_field( 'image', array( "alt" => "#", "output" => "raw") ) ;?>" class="block-link"  target="_blank">
-		    <?php echo types_render_field( 'image', array( "alt" => "#",  "size" => "han-preview-size" ) ) ;?>
-		</a>
-		<div class="caption-wrap">
-		    <p style="color: #000 ;">in caption wrap</p>
-		</div>
-		</div>
-	    </div>
-	<?php endwhile; ?>
+                <?php  } ?>
+		
+        <?php endwhile; ?>
     </section> <!-- ENDS #...  -->
 <?php endif; ?>
